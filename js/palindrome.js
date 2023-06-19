dd401c848cfd72ea8323bacbfb7d2a9c75655447
Atrocious <jackamorr@gmail.com>
2023-06-19 18:57:38 -0500

    Create js palindrome

---
+++ js/palindrome.js
@@ -0,0 +1,16 @@
+export function isPalindrome1(input) {
+  for (let i = 0; i < input.length; i++) {
+    if (input.charAt(i) != input.charAt(input.length - 1 - i)) {
+        return false;
+    }
+  }
+  return true;
+}
+
+export function isPalindrome2(input) {
+  const reversed = input.toString()
+    .split("")
+    .reverse()
+    .join("");
+  return input == reversed;
+}
