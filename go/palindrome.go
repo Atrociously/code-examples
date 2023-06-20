abd2ba33fc190a706bd2319d826d9eb75a8ab931
Atrocious <jackamorr@gmail.com>
2023-06-19 19:07:57 -0500

    Create go palindrome

---
+++ go/palindrome.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func IsPalindrome1(input string) bool {
+	for i := 0; i < len(input); i++ {
+		if input[i] != input[len(input) - 1 - i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsPalindrome1("abcba"))
+	fmt.Println(IsPalindrome1("abc"))
+}
